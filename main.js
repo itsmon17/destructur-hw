815749081caeec6e43f586966de19cfb48646113
Aimona <aimonadunaeva@gmail.com>
2024-03-25 23:40:36 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+//?    1-задание////////////
+
+// let user = {
+//   firstName: "Aimona",
+//   lastName: "Dunaeva",
+// };
+
+// function getFullName({ firstName = "Aimon", lastName = "Dunik" }) {
+//   return {
+//     firstName,
+//     lastName,
+//   };
+// }
+// console.log(getFullName(user));
+
+//?    2-задание////////////
+
+// let user = {
+//   x: 12,
+//   y: 45,
+// };
+
+// function printCoordinates({ x, y }) {
+//   return { x, y };
+// }
+
+// console.log(printCoordinates(user));
+
+//?    3-задание////////////
+
+const data = {
+  products: [
+    { id: 1, name: "Product 1" },
+    { id: 2, name: "Product 2" },
+  ],
+};
+
+function printProductNames(data) {
+  for (const { name } of data.products) {
+    console.log(`name: ${name}`);
+  }
+}
+
+printProductNames(data);
